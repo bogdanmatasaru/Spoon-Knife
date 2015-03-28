42b558d7f96e241f080929e37bce1e9a7240c66f
Bogdan Matasaru <bogdan.matasaru@yahoo.com>
2015-03-28 17:13:37 +0200

    [ADDED] ShipIO

---
+++ ShipIO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShipIO
+//
+//  Created by Admin on 28/03/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShipIO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShipIO
+//
+//  Created by Admin on 28/03/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
